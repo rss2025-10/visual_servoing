797b3a8b4da156a8b3c5e64ae575bd83d518d0d7
lexifol <lexifoland@gmail.com>
2025-03-12 22:58:02 -0400

    Added color segmentation

--- visual_servoing/visual_servoing/computer_vision/color_segmentation.py
+++ visual_servoing/visual_servoing/computer_vision/color_segmentation.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 #################### X-Y CONVENTIONS #########################
 # 0,0  X  > > > > >
@@ -22,7 +23,7 @@ def image_print(img):
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-def cd_color_segmentation(img, template):
+def cd_color_segmentation(img, template=None):
 	"""
 	Implement the cone detection using color segmentation algorithm
 	Input:
@@ -34,9 +35,35 @@ def cd_color_segmentation(img, template):
 	"""
 	########## YOUR CODE STARTS HERE ##########
 
-	bounding_box = ((0,0),(0,0))
+	# from GeeksForGeeks
 
-	########### YOUR CODE ENDS HERE ###########
+	eroded_img = cv2.erode(img, np.ones((5, 5), np.uint8))
+	image_print(eroded_img)
+	image_print(cv2.erode(img, np.ones((10, 10), np.uint8)))
+	dilated_img = cv2.dilate(eroded_img, np.ones((10,10), np.uint8))
+	# image_print(dilated_img)
+	hsv_version = cv2.cvtColor(dilated_img, cv2.COLOR_BGR2HSV)
+	# image_print(hsv_version)
+	orange_mask = cv2.inRange(hsv_version, np.array([0, 175, 150]), np.array([35, 255, 255]))
+	# image_print(orange_mask)
+	masked_img = cv2.bitwise_and(img, img, dst=None, mask=orange_mask)
+	# image_print(masked_img)
 
-	# Return bounding box
-	return bounding_box
+	contours, _ = cv2.findContours(orange_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	# image_print(cv2.drawContours(masked_img, contours, -1, (255, 0, 0), 2))
+	best_contour = None
+	best_contour_area = 0
+	for i in range(len(contours)):
+		this_contour = contours[i]
+		contour_area = cv2.contourArea(this_contour)
+		if contour_area > best_contour_area:
+			best_contour = this_contour
+			best_contour_area = contour_area
+
+	if best_contour is None:
+		raise AssertionError
+	else:
+		x_coord, y_coord, width, height = cv2.boundingRect(best_contour)
+		# image_print(cv2.rectangle(masked_img, (x_coord, y_coord), (x_coord + width, y_coord + height), (0, 255, 0), 2))
+
+	return (x_coord,y_coord), (x_coord + width, y_coord + height)
